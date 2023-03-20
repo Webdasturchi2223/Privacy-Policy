f0a61ecdb2b5f9a7d5c126446a794728c30daa43
Webdasturchi2223 <webdasturchi2223@gmail.com>
2023-03-20 13:13:48 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let btn = document.querySelector('.header__btn')
 let onOFFbtn = document.querySelector('.header__btn i')
 
 btn.addEventListener('click', function(){
-    document.querySelector('body').classList.toggle('night')
+    document.querySelector('html').classList.toggle('night')
     onOFFbtn.classList.toggle('fa-sun')
     onOFFbtn.classList.toggle('fa-moon')
 })
